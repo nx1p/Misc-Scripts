9a21b32207b854505279c923b808a2dceb68c582
9TailedCryptid <email@marci.tech>
2020-05-19 18:27:23 +0800

    Updated HTB script to comply with Python PEP-0008 style guidelines

--- HTB/Chall/Misc/misDIRection/script.py
+++ HTB/Chall/Misc/misDIRection/script.py
@@ -7,65 +7,79 @@
 #                                      #
 # Author: Marceline Kuo-Arrigo         #
 # Created: 13/12/2019                  #
-# Version: v1.1                        #
+# Last update: 19/5/2020                #
+# Version: v1.2                        #
 #                                      #
 ########################################
-##### Challange Description
-#  During an assessment of a unix system the HTB team found a suspicious directory. 
-#  They looked at everything within but couldn't find any files with malicious intent.
+# #### Challange Description
+#  During an assessment of a unix system the HTB team found
+#  a suspicious directory.
+#  They looked at everything within but couldn't find any files
+#  with malicious intent.
 #
-##### Script Description
-# This script will go through a list of directories some with files in them 
-# and sort them by the number of the file in the directory. Ignoring directories without files.
-# 
-# For example X directory could have a file in it labeled 4 and a Y directory could have a file in it labeled 3
-# So Y will go before X # 
-# So by doing this the script will put together what I hope is the flag for the challange.
+# #### Script Description
+# This script will go through a list of directories some with files in them
+# and sort them by the number of the file in the directory.
+# Ignoring directories without files.
+#
+# For example X directory could have a file in it labeled 4
+# and a Y directory could have a file in it labeled 3
+# So Y will go before X #
+# So by doing this the script will put together
+# what I hope is the flag for the challenge.
 #
 
 import subprocess
 import base64
 
+
 # User Introduction
-def intro(): 
+def intro():
     print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
     print("     misDIRection Script")
     print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n")
 
+
 # Provides a dict of files
 class DIRDict:
     def __runFind(self):
-      return subprocess.run("find .secret -type f | sed 's!.secret/!!'", shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE).stdout
+        return subprocess.run("find .secret -type f | sed 's!.secret/!!'",
+                              shell=True,
+                              check=True,
+                              universal_newlines=True,
+                              stdout=subprocess.PIPE).stdout
 
     def __SplitByLine(self):
-      return self.__runFind().splitlines() 
+        return self.__runFind().splitlines()
 
     def __splitEach(self, line):
-      splitted = line.split('/')
-      splitted.reverse()
-      return splitted
+        splitted = line.split('/')
+        splitted.reverse()
+        return splitted
 
     def __splitAllIntoKeyPair(self):
-      return map(self.__splitEach, self.__SplitByLine())
+        return map(self.__splitEach, self.__SplitByLine())
 
     def __convertToDict(self):
-      return dict(self.__splitAllIntoKeyPair())
+        return dict(self.__splitAllIntoKeyPair())
 
     def __getDIRDict(self):
-      return self.__convertToDict()
+        return self.__convertToDict()
 
     def __init__(self):
-      self.dict = self.__getDIRDict()
+        self.dict = self.__getDIRDict()
+
 
 def ConvertToString(dirdict, flag=''):
-  if len(flag) != len(dirdict):
-    flag += dirdict[str(len(flag)+1)]
-    return ConvertToString(dirdict, flag=flag)
-  elif len(flag) == len(dirdict):
-    return flag
+    if len(flag) != len(dirdict):
+        flag += dirdict[str(len(flag)+1)]
+        return ConvertToString(dirdict, flag=flag)
+    elif len(flag) == len(dirdict):
+        return flag
+
 
 def Log(msg):
-  print("   > "+msg)
+    print("   > "+msg)
 
 
 # Main function
@@ -76,8 +90,7 @@ def main():
     DecodedFlag = base64.b64decode(EncodedFlag)
     Log("Encoded Flag: "+EncodedFlag)
     Log("Decoded Flag: "+str(DecodedFlag))
-    
+
 
 if __name__ == "__main__":
     main()
-
