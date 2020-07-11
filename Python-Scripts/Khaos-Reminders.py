765646e812830976e4c2603791fc43cbd35bdcd3
9TailedCryptid <email@marci.tech>
2020-07-11 18:22:06 +0800

    Bot now counts down until the timeout

--- Python-Scripts/Khaos-Reminders.py
+++ Python-Scripts/Khaos-Reminders.py
@@ -5,7 +5,7 @@
 #       Khaos_Sys Reminder Script      #
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  #
 #                                      #
-# Author: Cryptid Khaos                #
+# Author: Cryptid&Electra Khaos        #
 # Created: 09/07/2020                  #
 # Last update: 09/07/2020              #
 # Version: v0.1                        #
@@ -14,21 +14,35 @@
 ########################################
 ## TO-DO
 # [X] Ask for confirmation to start up
-# [ ] Edit the confirmation message to show how long until time out
+# [X] Edit the confirmation message to show how long until time out
+#     - Time taken: (1h 12m)
 # [ ] Do the whole actual reminders stuff, sending X message after X time and all
 
 import discord
 import asyncio
+import time
 
 # Define Constants(-ish?)
 # Is CLIENT a constant?
 CLIENT = discord.Client()
 REMINDER_ID = 730746502112084008
 
+
 # Example usage: log("Booting up")
 # Use to output infomation to STDOUT adds on a > for style
 def log(msg):
     print(f"  > {msg}")
+
+
+# define function to edit the message for timeout
+async def edit_timeout(msg, start_timestamp):
+    await asyncio.sleep(10)
+    new_timeout = time.strftime('%HH:%MM:%SS',
+                                time.gmtime(3600 - (time.perf_counter()
+                                            - start_timestamp)))
+    await msg.edit(content=("\> system init? (✅/❌)\n"
+                        f'\> timeout in {new_timeout}\n'))
+    await edit_timeout(msg, start_timestamp)
 
 
 # Request confirmation from Khaos to Init
@@ -37,14 +51,17 @@ async def sys_init(client):
 
     log("Asking if system should init")
     msg = await channel.send(("\> system init? (✅/❌)\n"
-                              "\> timeout in 1 hour\n"))
+                              "\> timeout in 01H:00M:00S\n"))
 
     await msg.add_reaction("✅")
     await msg.add_reaction("❌")
 
+
     # define function to wait for reaction reply
     def check(reaction, user):
         return user != CLIENT.user and (str(reaction.emoji) == '✅' or str(reaction.emoji) == '❌')
+
+    edit_timeout_task = asyncio.create_task(edit_timeout(msg, time.perf_counter()))
 
     # wait for a reaction reply and then act according on the reply or lack of
     try:
@@ -56,11 +73,13 @@ async def sys_init(client):
     else:
         if(str(reaction.emoji) == '✅'):
             log("system will init now")
-            await channel.send('> reminder system booting up now')
+            await channel.send('\> reminder system booting up now')
         elif(str(reaction.emoji) == '❌'):
             log("system should not init, closing")
             await channel.send('\> reminder system shutting down now')
             await client.close()
+    finally:
+        edit_timeout_task.cancel()
 
 
 @CLIENT.event
