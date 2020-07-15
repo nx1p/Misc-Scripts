2a2c84bdf602ec993ef2e5856073912d1905eff5
9TailedCryptid <email@marci.tech>
2020-07-15 16:18:08 +0800

    Added basic morning routine reminders function and also changed the bot prompt a bit

--- Python-Scripts/Khaos-Reminders.py
+++ Python-Scripts/Khaos-Reminders.py
@@ -26,6 +26,7 @@ import time
 # Is CLIENT a constant?
 CLIENT = discord.Client()
 REMINDER_ID = 730746502112084008
+BOT_PROMPT = "**\>**"
 
 
 # Example usage: log("Booting up")
@@ -40,18 +41,29 @@ async def edit_timeout(msg, start_timestamp):
     new_timeout = time.strftime('%HH:%MM:%SS',
                                 time.gmtime(3600 - (time.perf_counter()
                                             - start_timestamp)))
-    await msg.edit(content=("\> system init? (✅/❌)\n"
-                        f'\> timeout in {new_timeout}\n'))
+    await msg.edit(content=(f'{BOT_PROMPT} system init? (✅/❌)\n'
+                        f'{BOT_PROMPT} timeout in {new_timeout}\n'))
     await edit_timeout(msg, start_timestamp)
 
+# Morning Reminders
+
+async def morning_reminders(client):
+    channel = client.get_channel(REMINDER_ID)
+    prompt = "**Reminder\>**"
+    reminders = (f'{BOT_PROMPT} Morning Reminders..\n'
+                 f'{prompt} Charge SmartWatch\n'
+                 f'{prompt} Use mouthwash and/or brush teeth'
+                 f'{prompt} Skim google Calander'
+                 f'{prompt} Eat Breakfast\n')
+    await channel.send(reminders)
 
 # Request confirmation from Khaos to Init
 async def sys_init(client):
     channel = client.get_channel(REMINDER_ID)
 
     log("Asking if system should init")
-    msg = await channel.send(("\> system init? (✅/❌)\n"
-                              "\> timeout in 01H:00M:00S\n"))
+    msg = await channel.send((f'{BOT_PROMPT} system init? (✅/❌)\n'
+                              f'{BOT_PROMPT} timeout in 01H:00M:00S\n'))
 
     await msg.add_reaction("✅")
     await msg.add_reaction("❌")
@@ -67,16 +79,16 @@ async def sys_init(client):
     try:
         reaction, user = await client.wait_for('reaction_add', timeout=3600.0, check=check)
     except asyncio.timeouterror:
-        await channel.send('> timed out, shutting down')
+        await channel.send(f'{BOT_PROMPT} timed out, shutting down')
         log("init question timed out")
         await client.close()
     else:
         if(str(reaction.emoji) == '✅'):
             log("system will init now")
-            await channel.send('\> reminder system booting up now')
+            await channel.send(f'{BOT_PROMPT} reminder system booting up now')
         elif(str(reaction.emoji) == '❌'):
             log("system should not init, closing")
-            await channel.send('\> reminder system shutting down now')
+            await channel.send(f'{BOT_PROMPT} reminder system shutting down now')
             await client.close()
     finally:
         edit_timeout_task.cancel()
@@ -86,6 +98,7 @@ async def sys_init(client):
 async def on_ready():
     log('We have logged in as {0.user}'.format(CLIENT))
     await sys_init(CLIENT)
+    await morning_reminders(CLIENT)
 
 
 @CLIENT.event
